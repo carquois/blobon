52a94ed8fd358d231fdff4d76f6a968e774826af
GabrielDancause <gabrieldancause@gmail.com>
2012-03-15 02:33:38 -0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -9,16 +9,7 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'checkdo_punn',                      # Or path to database file if using sqlite3.
-        'USER': 'checkdo_punn',                      # Not used with sqlite3.
-        'PASSWORD': 'a74108520A',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
+
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
