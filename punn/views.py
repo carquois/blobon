366ebfc8bc5e2f6ba4fe30a2f83004bac057eb45
Gabriel Dancause <gabrieldancause@gmail.com>
2012-03-16 19:23:59 -0400

    Change to the view

--- punn/views.py
+++ punn/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the poll index.")
+    return HttpResponse("Page d'accueil")
 
 def detail(request, punn_id):
-    return HttpResponse("You're looking at poll %s." % punn_id)
+    return HttpResponse("Affichage du punn %s." % punn_id)
