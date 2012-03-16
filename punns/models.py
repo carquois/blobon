2e407c1c00f8dda0c3d68d0bf7c845ac43664ab9
Gabriel Dancause <gabrieldancause@gmail.com>
2012-03-16 16:51:04 -0400

    Activate the admin

--- punns/models.py
+++ punns/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Punn(models.Model):
-    title = models.CharField(max_length=120)
+    title = models.CharField(max_length=140)
     karma = models.IntegerField()
     source = models.CharField(max_length=300)
     created = models.DateTimeField()
