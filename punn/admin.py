775215e69afd50d0e1b3911107182bb0a7a51259
Gabriel Dancause <gabrieldancause@gmail.com>
2012-04-01 02:55:44 -0400

    Change to gitignore

--- punn/admin.py
+++ punn/admin.py
@@ -1,6 +1,8 @@
 from punn.models import Punn
 from punn.models import User
+from comment.models import Comment
 from django.contrib import admin
 
 admin.site.register(Punn)
 admin.site.register(User)
+admin.site.register(Comment)
