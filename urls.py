29588c05cc1dba956662cae87286a503874f399d
Gabriel Dancause <gabrieldancause@gmail.com>
2012-04-18 00:46:33 -0400

    Changed punn/create for api/submit

--- urls.py
+++ urls.py
@@ -7,7 +7,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^$', 'punn.views.index'),
     url(r'^p/(?P<shorturl>.)/$', 'punn.views.detail'),
-    url(r'^p/create/$', 'punn.views.create'),
+    url(r'^api/submit/$', 'punn.views.create'),
 
     #TODO http://www.djangobook.com/en/beta/chapter12/
     url(r'^accounts/login/$',  login),
