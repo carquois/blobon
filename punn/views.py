2c4a935773c3abe39cc304584d60fad58d74b4bf
Gabriel Dancause <gabrieldancause@gmail.com>
2012-04-18 21:46:38 -0400

    Removed the /punn path in templates

--- punn/views.py
+++ punn/views.py
@@ -14,16 +14,17 @@ BASE62 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz"
 def index(request): 
     latest_punn_list = Punn.objects.all().order_by('pub_date')[:24]
     current_site = Site.objects.get(id=settings.SITE_ID)
-    return render_to_response('punn/index.html', {'site': current_site, 'latest_punn_list': latest_punn_list})
+    return render_to_response('index.html', {'site': current_site, 'latest_punn_list': latest_punn_list})
 
 def profile_page(request, user):
     u = get_object_or_404(User, username=user)
     current_site = Site.objects.get(id=settings.SITE_ID)
     latest_punn_list = Punn.objects.filter(author=u).order_by('pub_date')[:24]
-    return render_to_response('punn/profile.html', {'user': u, 'site': current_site, 'latest_punn_list': latest_punn_list})
+    return render_to_response('profile.html', {'user': u, 'site': current_site, 'latest_punn_list': latest_punn_list})
 
 def create(request): 
-    return render_to_response('punn/create.html', {})
+
+    return render_to_response('create.html', {})
 
 def detail(request, shorturl):
     current_site = Site.objects.get(id=settings.SITE_ID)
@@ -34,7 +35,7 @@ def detail(request, shorturl):
     latest_repunn_list = Punn.objects.filter(original_punn=p.id).order_by('pub_date')[:6]
     top_comments = Comment.objects.all().order_by('karma')[:6]
     tag_cloud = p.tags.all()[:6]
-    return render_to_response('punn/single.html', {'punn': p, 'user': u,  'site': current_site, 'tag_cloud': tag_cloud, 'latest_punn_list': latest_punn_list, 'latest_repunn_list': latest_repunn_list, 'top_comments': top_comments})
+    return render_to_response('single.html', {'punn': p, 'user': u,  'site': current_site, 'tag_cloud': tag_cloud, 'latest_punn_list': latest_punn_list, 'latest_repunn_list': latest_repunn_list, 'top_comments': top_comments})
 
 
 def baseconvert(number,fromdigits,todigits):
