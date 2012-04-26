108a3d0e292e72d8eecb9b3b24dc74fda751e07a
Gabriel Dancause <gabrieldancause@gmail.com>
2012-04-25 22:04:08 -0400

    Added api/ for functions

--- urls.py
+++ urls.py
@@ -9,11 +9,10 @@ urlpatterns = patterns('',
     url(r'^p/(?P<shorturl>.)/$', 'punn.views.single'),
     url(r'^t/(?P<shorturl>.)/$', 'punn.views.tag'),
     url(r'^c/(?P<shorturl>.)/$', 'punn.views.comment'),
+    url(r'^api/signup/$', 'punn.views.signup'),
     url(r'^api/submit/$', 'punn.views.submit'),
-
-    #TODO http://www.djangobook.com/en/beta/chapter12/
-    url(r'^login/$',  login),
-    url(r'^logout/$', logout),
+    url(r'^api/login/$',  login),
+    url(r'^api/logout/$', logout),
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^(?P<user>[^/]+)/$', 'punn.views.profile_page'), 
