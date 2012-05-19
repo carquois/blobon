4dc8c794baa33359ff8f227ae8a064e691c8ea80
Gabriel Dancause <gabrieldancause@gmail.com>
2012-05-18 20:20:36 -0400

    Added the permalink and base62id mechanism to comment

--- punn/models.py
+++ punn/models.py
@@ -109,7 +109,7 @@ class Punn(models.Model):
 
 class Comment(models.Model):
     content = models.CharField(max_length=10000)
-    base64id = models.CharField(max_length=140, blank=True)
+    base62id = models.CharField(max_length=140, blank=True)
     author = models.ForeignKey(User)
     punn = models.ForeignKey(Punn)
     karma = models.IntegerField()
@@ -118,5 +118,12 @@ class Comment(models.Model):
     pub_date = models.DateTimeField('date published')
     def __unicode__(self):
         return self.content
-
+    def save(self):
+        super(Comment, self).save()
+        if not self.base62id:
+            self.base62id = baseconvert(str(self.id),BASE10,BASE62)
+            self.save()
+    @models.permalink
+    def get_absolute_url(self):
+        return ('punn.views.comment', [str(self.base62id)])
 
