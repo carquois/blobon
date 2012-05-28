746a1cec4624ece11801cd6016a6b2bddb2db4d2
Gabriel Dancause <gabrieldancause@gmail.com>
2012-05-28 15:15:51 -0400

    Removed the old foreign keys image,story, tweets

--- punn/admin.py
+++ punn/admin.py
@@ -2,21 +2,10 @@ from punn.models import Punn
 from punn.models import UserProfile
 from punn.models import Comment
 from punn.models import Tag
-from punn.models import Image
-from punn.models import Video
-from punn.models import Audio
-from punn.models import Tweet
-from punn.models import Story
 from django.contrib import admin
 
 admin.site.register(Punn)
 admin.site.register(UserProfile)
 admin.site.register(Comment)
 admin.site.register(Tag)
-admin.site.register(Image)
-admin.site.register(Video)
-admin.site.register(Audio)
-admin.site.register(Tweet)
-admin.site.register(Story)
-
 
