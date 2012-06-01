5c8fe0593ea056ab59c2e428a6d59ebda02f9c61
Gabriel Dancause <gabrieldancause@gmail.com>
2012-06-01 12:56:53 -0400

    Removed the old apps

--- punn_it/punn/admin.py
+++
@@ -1,9 +0,0 @@
-from punn.models import Punn
-from account.models import UserProfile
-from comment.models import Comment
-from django.contrib import admin
-
-admin.site.register(Punn)
-admin.site.register(UserProfile)
-admin.site.register(Comment)
-
