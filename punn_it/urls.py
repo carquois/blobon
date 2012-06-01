614b6bd353e8b5c24d44816186c11c6ba558fa1d
Gabriel Dancause <gabrieldancause@gmail.com>
2012-06-01 15:11:29 -0400

    Changed from punns to punn

--- punn_it/urls.py
+++ punn_it/urls.py
@@ -5,17 +5,17 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', 'punn.views.index'),
-    url(r'^p/(?P<shorturl>.)/$', 'punn.views.single'),
-    url(r'^s/(?P<shorturl>.)/$', 'punn.views.tag'),
-    url(r'^c/(?P<shorturl>.)/$', 'punn.views.comment'),
-    url(r'^signup/$', 'punn.views.signup'),
-    url(r'^submit/$', 'punn.views.submit'),
+    url(r'^$', 'punns.views.index'),
+    url(r'^p/(?P<shorturl>.)/$', 'punns.views.single'),
+    url(r'^s/(?P<shorturl>.)/$', 'punns.views.tag'),
+    url(r'^c/(?P<shorturl>.)/$', 'punns.views.comment'),
+    url(r'^signup/$', 'punns.views.signup'),
+    url(r'^submit/$', 'punns.views.submit'),
     url(r'^login/$',  login),
     url(r'^logout/$', logout),
-    url(r'^settings/profile/$', 'punn.views.edit_profile'),
+    url(r'^settings/profile/$', 'punns.views.edit_profile'),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^(?P<user>[^/]+)/$', 'punn.views.profile_page'), 
+    url(r'^(?P<user>[^/]+)/$', 'punns.views.profile_page'), 
 )
 
