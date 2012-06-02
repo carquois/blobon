1d02832e618a0a83ab3a93b1df18e4469affbd1c
Gabriel Dancause <gabrieldancause@gmail.com>
2012-06-02 15:58:07 -0400

    Started the locals() trick on many functions

--- punn_it/punns/views.py
+++ punn_it/punns/views.py
@@ -43,10 +43,8 @@ def edit_profile(request):
 
 def index(request): 
     latest_punn_list = Punn.objects.all().order_by('-pub_date')[:24]
-    current_site = Site.objects.get(id=settings.SITE_ID)
-    return render_to_response('index.html', 
-                              {'site': current_site, 'latest_punn_list': latest_punn_list}, 
-                              context_instance=RequestContext(request))
+    site = Site.objects.get(id=settings.SITE_ID)
+    return render_to_response('index.html', locals(), context_instance=RequestContext(request))
 
 def tag(request, shorturl):
     return HttpResponse("Tag page")
@@ -67,15 +65,15 @@ def logout(request):
     return HttpResponseRedirect("/")
 
 def comment(request, shorturl):
-    c = get_object_or_404(Comment, base62id=shorturl)
-    latest_reply_list = Comment.objects.filter(parent=c.id).order_by('pub_date')[:6]
-    return render_to_response('comment.html', {'comment': c, 'latest_reply_list': latest_reply_list})
+    comment = get_object_or_404(Comment, base62id=shorturl)
+    latest_reply_list = Comment.objects.filter(parent=comment.id).order_by('pub_date')[:6]
+    return render_to_response('comment.html', locals())
 
 def profile_page(request, user):
-    u = get_object_or_404(User, username=user)
-    current_site = Site.objects.get(id=settings.SITE_ID)
-    latest_punn_list = Punn.objects.filter(author=u).order_by('pub_date')[:24]
-    return render_to_response('profile.html', {'user': u, 'site': current_site, 'latest_punn_list': latest_punn_list})
+    user = get_object_or_404(User, username=user)
+    site = Site.objects.get(id=settings.SITE_ID)
+    latest_punn_list = Punn.objects.filter(author=user).order_by('-pub_date')[:24]
+    return render_to_response('profile.html', locals(),context_instance=RequestContext(request))
 
 #TODO enctype="multipart/form-data" dans le <form>
 @login_required
@@ -96,13 +94,13 @@ def submit(request):
       return render_to_response('submit.html', {})
 
 def single(request, shorturl):
-    current_site = Site.objects.get(id=settings.SITE_ID)
-    p = get_object_or_404(Punn, base62id=shorturl)
-    u = p.author
-    latest_punn_list = Punn.objects.filter(pub_date__gt=p.pub_date).order_by('pub_date').exclude(pk=p.id)[:6]
-    latest_repunn_list = Punn.objects.filter(original_punn=p.id).order_by('pub_date')[:6]
+    site = Site.objects.get(id=settings.SITE_ID)
+    punn = get_object_or_404(Punn, base62id=shorturl)
+    user = punn.author
+    latest_punn_list = Punn.objects.filter(pub_date__gt=punn.pub_date).order_by('pub_date').exclude(pk=punn.id)[:6]
+    latest_repunn_list = Punn.objects.filter(original_punn=punn.id).order_by('pub_date')[:6]
     top_comments = Comment.objects.all().order_by('karma')[:6]
-    return render_to_response('single.html', {'punn': p, 'user': u,  'site': current_site, 'latest_punn_list': latest_punn_list, 'latest_repunn_list': latest_repunn_list, 'top_comments': top_comments}, context_instance=RequestContext(request))
+    return render_to_response('single.html', locals(), context_instance=RequestContext(request))
 
 
 
