34d2595f39d59739d02f6c0df4fc64974c6c3b2a
Gabriel Dancause <gabrieldancause@gmail.com>
2012-06-02 20:31:00 -0400

    Added top

--- punn_it/punns/views.py
+++ punn_it/punns/views.py
@@ -16,6 +16,10 @@ from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
 from django.db.models import Count
 
+def top(request):
+    latest_punn_list = Punn.objects.annotate(number_of_comments=Count('comment')).order_by('karma')[:24]
+    site = Site.objects.get(id=settings.SITE_ID)
+    return render_to_response('index.html', locals(), context_instance=RequestContext(request))
 
 def signup(request):
     if request.method == 'POST':
@@ -41,8 +45,7 @@ def edit_profile(request):
     return render_to_response('edit_profile.html', locals())
 
 def index(request): 
-    latest_punn_list = Punn.objects.all().order_by('-pub_date')[:24]
-    latest_punn_list = Punn.objects.annotate(number_of_comments=Count('comment'))[:24]
+    latest_punn_list = Punn.objects.annotate(number_of_comments=Count('comment')).order_by('-pub_date')[:24]
     site = Site.objects.get(id=settings.SITE_ID)
     return render_to_response('index.html', locals(), context_instance=RequestContext(request))
 
