8ff2754be51d492553f902b69c9b1e6fe1eb60bf
Gabriel Dancause <gabrieldancause@gmail.com>
2012-06-04 17:39:26 -0400

    Started the common used fucntions

--- punn_it/urls.py
+++ punn_it/urls.py
@@ -1,22 +1,26 @@
 from django.conf.urls.defaults import patterns, include, url
-from django.contrib.auth.views import login, logout
 from django.contrib import admin
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^login/$', 'django.contrib.auth.views.login'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout',
+                          {'next_page': '/'}),
+    url(r'^settings/profile/$', 'punns.views.edit_profile'),
+    #TODO hook the password change page
+    url(r'^settings/password/$', 'django.contrib.auth.views.password_change',
+                          {'post_change_redirect': '/settings/profile/'}),
+    url(r'', include('social_auth.urls')),
     url(r'^$', 'punns.views.index'),
     url(r'^p/(?P<shorturl>.)/$', 'punns.views.single'),
     url(r'^s/(?P<shorturl>.)/$', 'punns.views.tag'),
     url(r'^c/(?P<shorturl>.)/$', 'punns.views.comment'),
     url(r'^top/$', 'punns.views.top'),
+    url(r'^top/day$', 'punns.views.top_day'),
     url(r'^signup/$', 'punns.views.signup'),
     url(r'^submit/$', 'punns.views.submit'),
-    url(r'^login/$',  login),
-    url(r'^logout/$', logout),
-    url(r'^settings/profile/$', 'punns.views.edit_profile'),
 
-    url(r'', include('social_auth.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^(?P<user>[^/]+)/$', 'punns.views.profile_page'), 
