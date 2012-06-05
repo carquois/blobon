a1b1434fe8c5f2f5c30328e2938c48d32cedd217
Gabriel Dancause <gabrieldancause@gmail.com>
2012-06-05 01:29:23 -0400

    Password reset mechanism now works. Yay!

--- punn_it/urls.py
+++ punn_it/urls.py
@@ -8,11 +8,20 @@ urlpatterns = patterns('',
     url(r'^logout/$', 'django.contrib.auth.views.logout',
                           {'next_page': '/'}),
     url(r'^settings/profile/$', 'punns.views.edit_profile'),
-    #TODO hook the password change page
     url(r'^settings/password/$', 'django.contrib.auth.views.password_change',
                           {'post_change_redirect': '/settings/password/password_reset_confirmation/'}),
     url(r'^settings/password/password_reset_confirmation/$', 'django.contrib.auth.views.password_change_done'),
-    url(r'', include('social_auth.urls')),
+    url(r'^password_reset/$', 'django.contrib.auth.views.password_reset', 
+                          {'template_name': 'registration/password_reset_form.html',
+                           'email_template_name': 'registration/password_reset_email.html'}),
+    url(r'^password_reset/done/$', 'django.contrib.auth.views.password_reset_done', 
+			  {'template_name': 'registration/password_reset_done.html'}),
+    url(r'^reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 'django.contrib.auth.views.password_reset_confirm', 
+                          {'template_name': 'registration/password_reset_confirm.html'}),
+    url(r'^reset/done/$', 'django.contrib.auth.views.password_reset_complete', 
+			  {'template_name': 'registration/password_reset_complete.html'}),
+
+#url(r'', include('social_auth.urls')),
     url(r'^$', 'punns.views.index'),
     url(r'^p/(?P<shorturl>.)/$', 'punns.views.single'),
     url(r'^s/(?P<shorturl>.)/$', 'punns.views.tag'),
@@ -25,5 +34,6 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^(?P<user>[^/]+)/$', 'punns.views.profile_page'), 
+
 )
 
