57115f667b6f3bb81237b88291a4e0afb85a87f0
Gabriel Dancause <gabrieldancause@gmail.com>
2012-06-06 11:39:09 -0400

    Removed the useless login view

--- punn_it/punns/views.py
+++ punn_it/punns/views.py
@@ -54,16 +54,6 @@ def index(request):
 def tag(request, shorturl):
     return HttpResponse("Tag page")
 
-def login(request):
-    username = request.POST['username']
-    password = request.POST['password']
-    user = auth.authenticate(username=username, password=password)
-    if user is not None and user.is_active:
-        auth.login(request, user)
-        return HttpResponseRedirect("/fdsa/")
-    else:
-        return HttpResponseRedirect("/account/invalid/")
-
 @login_required
 def logout(request):
     auth.logout(request)
