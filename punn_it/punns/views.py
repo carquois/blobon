eb7dd651864824c34858c7d6229287cbe79b6b63
Gabriel Dancause <gabrieldancause@gmail.com>
2012-10-02 01:34:22 -0400

    Fixed #39

--- punn_it/punns/views.py
+++ punn_it/punns/views.py
@@ -1,3 +1,4 @@
+from django.views.decorators.cache import cache_page
 from django.contrib.auth.models import User
 from punns.models import Punn, PunnForm
 from accounts.models import UserProfile
@@ -106,6 +107,7 @@ def done(request):
     }
     return render_to_response('done.html', ctx, RequestContext(request))
 
+@cache_page(60 * 15)
 def profile_page(request, user):
     host = request.META['HTTP_HOST']
     url = 'http://%s/' % (host)
@@ -160,7 +162,7 @@ def submit(request):
       form = PunnForm()
     return render_to_response('submit.html', locals(), context_instance=RequestContext(request))
 
-
+@cache_page(60 * 30)
 def single(request, shorturl):
     punn = get_object_or_404(Punn, base62id=shorturl)
     user = punn.author
