fe2ee5fc0544eb554dd50c0d48ce25a27ecb6ce9
Gabriel Dancause <gabrieldancause@gmail.com>
2012-10-16 00:39:04 -0400

    Fixed #11. La prochaine étape sera de prendre une app avec oauth2 pour augmenter le nombre de request par jours. Pour utiliser, juste a lancer manage.py countviews

--- punn_it/punns/management/commands/countviews.py
+++ punn_it/punns/management/commands/countviews.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand, CommandError
 from punns.models import Punn
 import gdata.analytics.client  
+import time
 
 class Command(BaseCommand):
   help = 'Count the views'
@@ -18,15 +19,16 @@ class Command(BaseCommand):
     )
     token = my_client.auth_token
     account_query = gdata.analytics.client.AccountFeedQuery()
-    for p in Punn.objects.filter(status='P').order_by('-pub_date')[:5]:
+    for p in Punn.objects.filter(status='P').order_by('-pub_date')[:1500]:
       data_query = gdata.analytics.client.DataFeedQuery({
         'ids': 'ga:64078171',
         'metrics': 'ga:pageviews',
         'filters': 'ga:pagePath==/p/%s/' % p.base62id,
         'start-date': '2012-10-01',
-        'end-date': '2012-10-15',
+        'end-date': '%s' % time.strftime("%Y-%m-%d"),
       }) 
       feed = my_client.GetDataFeed(data_query)
       result = [(x.name, x.value) for x in feed.entry[0].metric]
-      self.stdout.write(str(result))
+      p.views = x.value
+      p.save()
     self.stdout.write('Success')
