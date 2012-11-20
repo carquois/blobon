744e02c1894292cdcd46c50b419c2f2f2d969426
Gabriel Dancause <gabrieldancause@gmail.com>
2012-11-19 20:11:10 -0500

    Commit initial du code des tags. #87

--- punn_it/punns/admin.py
+++ punn_it/punns/admin.py
@@ -1,4 +1,5 @@
 from punns.models import Punn
+from punns.models import Tags
 from accounts.models import UserProfile
 from comments.models import Comment
 from images.models import Image
@@ -6,6 +7,7 @@ from votes.models import CommentVote, PunnVote
 from django.contrib import admin
 
 admin.site.register(Punn)
+admin.site.register(Tags)
 admin.site.register(UserProfile)
 admin.site.register(Comment)
 admin.site.register(CommentVote)
