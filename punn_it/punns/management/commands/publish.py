d4756cbf34012c6cee6aafd77f19afa18ae5918b
Gabriel Dancause <gabrieldancause@gmail.com>
2012-12-09 23:49:20 -0500

    Le publish est refractoré. #117

--- punn_it/punns/management/commands/publish.py
+++ punn_it/punns/management/commands/publish.py
@@ -3,6 +3,15 @@ from django.core.management.base import BaseCommand, CommandError
 from punns.models import Punn
 from accounts.models import UserProfile
 
+def publish(frequency):
+  for u in UserProfile.objects.all().distinct():
+    if u.publication_frequency == frequency:
+      p = Punn.objects.filter(author = u.user).filter(status='D')
+      if p.count() > 0:
+        e = Punn.objects.get(id=p[0].pk)
+        e.status = 'P'
+        e.save()
+
 class Command(BaseCommand):
   args = '<frequency frequency ...>'
   help = 'Publish an article'
@@ -10,19 +19,11 @@ class Command(BaseCommand):
   def handle(self, *args, **options):
     for frequency in args:
       if frequency == '15m':
-        for u in UserProfile.objects.all().distinct():
-          if u.publication_frequency == '15m':
-            p = Punn.objects.filter(author = u.user).filter(status='D')
-            if p.count() > 0:
-              e = Punn.objects.get(id=p[0].pk)
-              e.status = 'P'
-              e.save()
+        publish('15m')
       if frequency == '30m':
-        for u in UserProfile.objects.all().distinct():
-          if u.publication_frequency == '30m':
-            p = Punn.objects.filter(author = u.user).filter(status='D')
-            if p.count() > 0:
-              e = Punn.objects.get(id=p[0].pk)
-              e.status = 'P'
-              e.save()
+        publish('30m')
+      if frequency == '1h':
+        publish('1h')
+      if frequency == '3h':
+        publish('3h')
 
