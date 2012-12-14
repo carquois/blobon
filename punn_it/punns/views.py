e2f59b4406342e6fa8e46409c53b2227d03135ac
Gabriel Dancause <gabrieldancause@gmail.com>
2012-12-13 19:25:17 -0500

    Ai enlevé le système de redirection dURL

--- punn_it/punns/views.py
+++ punn_it/punns/views.py
@@ -31,29 +31,7 @@ from django.template import RequestContext
 from django.views.decorators.cache import cache_page
 
 def index(request, draft):
-    #Test pour voir si la requete est faite à l'adresse principale
-    if request.META['HTTP_HOST'] == settings.MAIN_SITE: 
-      return HttpResponse("You're at the main site index.")
-    #Test pour voir si la requete est faite à une adresse entrée dans le profil d'un utilisateur
-    elif UserProfile.objects.filter(domain='http://%s/' % (request.META['HTTP_HOST'])).exists():
-      user = UserProfile.objects.get(domain='http://%s/' % (request.META['HTTP_HOST'])).user
-      #Test to see if the variable is a draft or not
-      if draft == False:
-        punns = paginate(request,
-                         Punn.objects.filter(author=user).filter(status='P').order_by('-pub_date'),
-                         20)
-      else:
-        punns = paginate(request,
-                         Punn.objects.filter(author=user).filter(status='D').order_by('-pub_date'),
-                         20)
-      return render_to_response('profile.html', 
-                               {'user': user, 
-                                'url': 'http://%s/' % (request.META['HTTP_HOST']), 
-                                'punns': punns}, 
-                               context_instance=RequestContext(request))
-    #Il s'agit d'une adresse qui pointe vers nous sans que l'URL soit entre dans le profil
-    else:
-      #En cas de l'adresse ne semble pas présente dans notre db, présenter checkdonc.ca
+      #Affichage de checkdonc.ca
       user = UserProfile.objects.get(pk=3)
       #Test to see if the variable is a draft or not
       if draft == False:
