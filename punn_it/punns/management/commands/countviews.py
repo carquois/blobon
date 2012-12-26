88921b2ddd0e2829e88eb3863c7995c3dba73c06
Gabriel Dancause <gabrieldancause@gmail.com>
2012-12-26 02:39:54 -0500

    Ai enlevé les bouts commentés #131

--- punn_it/punns/management/commands/countviews.py
+++ punn_it/punns/management/commands/countviews.py
@@ -49,27 +49,4 @@ class Command(BaseCommand):
         # Handle Auth errors.
         print ('The credentials have been revoked or expired, please re-run '
                'the application to re-authorize')
-#    SOURCE_APP_NAME = 'Blobon view counter'
-#    my_client = gdata.analytics.client.AnalyticsClient(source=SOURCE_APP_NAME)
-#    my_client.client_login(
-#        'gabrieldancause@gmail.com',
-#        'a74108520S',
-#        source=SOURCE_APP_NAME,
-#        service=my_client.auth_service,
-#        account_type = 'GOOGLE',
-#    )
-#    token = my_client.auth_token
-#    account_query = gdata.analytics.client.AccountFeedQuery()
-#    for p in Punn.objects.filter(status='P').order_by('-pub_date')[:1500]:
-#      data_query = gdata.analytics.client.DataFeedQuery({
-#        'ids': 'ga:64078171',
-#        'metrics': 'ga:pageviews',
-#        'filters': 'ga:pagePath==/p/%s/' % p.base62id,
-#        'start-date': '2012-10-01',
-#        'end-date': '%s' % time.strftime("%Y-%m-%d"),
-#      }) 
-#      feed = my_client.GetDataFeed(data_query)
-#      result = [(x.name, x.value) for x in feed.entry[0].metric]
-#      p.views = x.value
-#      p.save()
     self.stdout.write('Success')
