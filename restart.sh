1f21c49a09e4dc2cbabcc9d3aa4d6b873f37bd92
Gabriel Dancause <gabrieldancause@gmail.com>
2013-01-17 23:16:59 -0500

    #148 Bouton à droite et différent si non logué

--- restart.sh
+++ restart.sh
@@ -1,3 +1,5 @@
+cd /home/web/punn.it
+git pull
 /home/web/punn.it/bin/python /home/web/punn.it/punn_it/manage.py migrate punns
 /home/web/punn.it/bin/python /home/web/punn.it/punn_it/manage.py migrate accounts 
 /home/web/punn.it/bin/python /home/web/punn.it/punn_it/manage.py collectstatic --noinput
