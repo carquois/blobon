249b728bec21de543a1f074c7af50843e5f6f6d8
Benoit Domingue <bedingue@gmail.com>
2013-01-27 10:52:37 -0500

    Make deploy script run with 'web' user. Deploy script actually works now :)

--- fabfile.py
+++ fabfile.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 import os
 from fabric.api import *
 from contextlib import contextmanager as _contextmanager
-from punn_it import local_settings as settings
+from punn_it import local_settings as django_django_settings
 
 env.use_ssh_config= True
 PROJECT_DIR = os.path.abspath(os.path.dirname(__file__))
@@ -12,11 +12,11 @@ PROJECT_DIR = os.path.abspath(os.path.dirname(__file__))
 This should be in ~/.ssh/config
 
 Host cdc-prod
-    User root
+    User web
     Hostname 66.175.208.206
 
 Host cdc-dev
-    User root
+    User web
     Hostname 50.116.51.59
 
 """
@@ -57,11 +57,11 @@ def get_db():
 
     TODO : add support for POSTGRES
     """
-    require("hosts", provided_by=["dev","prod"])
+    require("hosts", provided_by=["dev","prod"], used_for="environment variable initialization")
     
     msg = prompt("Are you sure you want to replace content of local database with the database from '%s'?" % env.hosts[0], default="y/n")
     if msg == "y":
-        if settings.DATABASES['default']['ENGINE'] == "django.db.backends.mysql":
+        if django_settings.DATABASES['default']['ENGINE'] == "django.db.backends.mysql":
             get_mysql_db()
         else:
             raise Exception("Database engine not supported")
@@ -80,11 +80,11 @@ def get_mysql_db():
 
     #load sql
     local('gunzip < /tmp/%(filename)s.sql.gz | mysql -u %(user)s -h %(host)s -p%(password)s %(database)s' % {
-        'user' : settings.DATABASES['default']['USER'],
-        'password' : settings.DATABASES['default']['PASSWORD'],
-        'host' : settings.DATABASES['default']['HOST'],
-        'password' : settings.DATABASES['default']['PASSWORD'],
-        'database' : settings.DATABASES['default']['NAME'],
+        'user' : django_settings.DATABASES['default']['USER'],
+        'password' : django_settings.DATABASES['default']['PASSWORD'],
+        'host' : django_settings.DATABASES['default']['HOST'],
+        'password' : django_settings.DATABASES['default']['PASSWORD'],
+        'database' : django_settings.DATABASES['default']['NAME'],
         'filename' : filename,
     })
     
@@ -99,11 +99,11 @@ def get_all_media():
     """
     will fetch the media for remote project using rsync
     """
-    require("hosts", provided_by=["dev","prod"])
+    require("hosts", provided_by=["dev","prod"], used_for="environment variable initialization")
     
     remote_dir = os.path.join(env.project_root, "media")
     local_dir = os.path.join(PROJECT_DIR, "punn_it", "media")
-    local("rsync -ave ssh root@%(host)s:/%(remote_dir)s/ %(local_dir)s/ --exclude \"cache\"" % {
+    local("rsync -ave ssh web@%(host)s:/%(remote_dir)s/ %(local_dir)s/ --exclude \"cache\"" % {
         'host' : env.host,
         'remote_dir' : remote_dir,
         'local_dir' : local_dir,
@@ -115,7 +115,7 @@ def get_recent_media():
     """
     will fetch the media for remote project using rsync
     """
-    require("hosts", provided_by=["dev","prod"])
+    require("hosts", provided_by=["dev","prod"], used_for="environment variable initialization")
         
     remote_dir = os.path.join(env.project_root, "media")
     local_dir = os.path.join(PROJECT_DIR, "punn_it", "media")
@@ -142,7 +142,7 @@ def update_local():
     """
     Will fetch db and media
     """
-    require("hosts", provided_by=["dev","prod"])
+    require("hosts", provided_by=["dev","prod"], used_for="environment variable initialization")
     
     execute(get_db)
     execute(get_recent_media)
@@ -153,20 +153,20 @@ def deploy():
     """
     Deploys the current project : hg push, hg update, manage.py collectstatic, manage.py migrate and restart gunicorn
     """
-    require("hosts", provided_by=["dev","prod"])
-    
+    require("hosts", provided_by=["dev","prod"], used_for="environment variable initialization")
     local("git push")
 
     with cd(env.project_root):
         run("git rev-parse HEAD")
         run("git pull")
-    with virtualenv():
-        with cd(env.directory):
-            run('pip install -r requirements.txt')
+        with virtualenv():
+            with cd(env.directory):
+                run('pip install -r requirements.txt')
 
-        run('python manage.py collectstatic')
-        run('python manage.py migrate')
-        run('supervisorctl restart %s' % env.gunicorn_name)
+            run('python manage.py collectstatic --noinput')
+            run('python manage.py migrate')
+        
+    sudo('supervisorctl restart %s' % env.gunicorn_name, shell=False)
 
 
 
