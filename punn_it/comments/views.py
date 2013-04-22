15984deec283a7dc495b9440de5000a3be4d9cd2
Gabriel Dancause <gabrieldancause@gmail.com>
2013-04-22 13:25:05 -0400

    Les utilisateurs peuvent maintenant effacer leurs commentaires

--- punn_it/comments/views.py
+++ punn_it/comments/views.py
@@ -1,1 +1,15 @@
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponseRedirect
+
+from comments.models import Comment
+from punns.models import Punn
+
+@login_required
+def delete(request, id):
+      comment = get_object_or_404(Comment, id=id)
+      punn = comment.punn
+      if request.user == comment.author:
+        comment.delete()
+      return HttpResponseRedirect( punn.get_absolute_url() )
+
