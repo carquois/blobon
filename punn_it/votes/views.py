8c350ee2ba015a3b447067b2644f055517dc88eb
Gabriel Dancause <gabrieldancause@gmail.com>
2013-05-02 18:03:30 -0400

    Ai continué à enelver le ajax. #185 Les votes commentaires sont encore brisés

--- punn_it/votes/views.py
+++ punn_it/votes/views.py
@@ -1,1 +1,53 @@
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponseRedirect
+
+from comments.models import Comment
+from punns.models import Punn
+from votes.models import CommentVote, PunnVote
+
+@login_required
+def up(request, id):
+      punn = get_object_or_404(Punn, id=id)
+      v = PunnVote.objects.filter(punn=punn).filter(user=request.user).filter(vote='U')
+      if v.count() == 0:
+        vote = PunnVote(punn=punn, user=request.user, vote='U')
+        vote.save()
+      elif v.count() == 1:
+        v[0].delete()
+      return HttpResponseRedirect( punn.get_absolute_url() )
+
+@login_required
+def down(request, id):
+      punn = get_object_or_404(Punn, id=id)
+      v = PunnVote.objects.filter(punn=punn).filter(user=request.user).filter(vote='D')
+      if v.count() == 0:
+        vote = PunnVote(punn=punn, user=request.user, vote='D')
+        vote.save()
+      elif v.count() == 1:
+        v[0].delete()
+      return HttpResponseRedirect( punn.get_absolute_url() )
+
+@login_required
+def voteup(request, id):
+      comment = get_object_or_404(Comment, id=id)
+      punn = comment.punn
+      v = CommentVote.objects.filter(comment=comment).filter(user=request.user).filter(vote='U')
+      if v.count() == 0:
+        vote = CommentVote(comment=comment, user=request.user, vote='U')
+        vote.save()
+      elif v.count() == 1:
+        v[0].delete() 
+      return HttpResponseRedirect( punn.get_absolute_url() )
+
+@login_required
+def votedown(request, id):
+      comment = get_object_or_404(Comment, id=id)
+      punn = comment.punn
+      v = CommentVote.objects.filter(comment=comment).filter(user=request.user).filter(vote='D')
+      if v.count() == 0:
+        vote = CommentVote(comment=comment, user=request.user, vote='D')
+        vote.save()
+      elif v.count() == 1:
+        v[0].delete() 
+      return HttpResponseRedirect( punn.get_absolute_url() )
