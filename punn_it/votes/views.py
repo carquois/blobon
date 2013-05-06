b2b06fdfe69c9cf5b24c247d3b5c48a5934d14ea
Gabriel Dancause <gabrieldancause@gmail.com>
2013-05-06 13:26:57 -0400

    Fixed #185. Les votes se passent maintenant en non-ajax et le code est tout refractoré

--- punn_it/votes/views.py
+++ punn_it/votes/views.py
@@ -9,45 +9,52 @@ from votes.models import CommentVote, PunnVote
 @login_required
 def up(request, id):
       punn = get_object_or_404(Punn, id=id)
-      v = PunnVote.objects.filter(punn=punn).filter(user=request.user).filter(vote='U')
-      if v.count() == 0:
-        vote = PunnVote(punn=punn, user=request.user, vote='U')
-        vote.save()
-      elif v.count() == 1:
-        v[0].delete()
+      punnvote(request, 'U', punn)
       return HttpResponseRedirect( punn.get_absolute_url() )
 
 @login_required
 def down(request, id):
       punn = get_object_or_404(Punn, id=id)
-      v = PunnVote.objects.filter(punn=punn).filter(user=request.user).filter(vote='D')
-      if v.count() == 0:
-        vote = PunnVote(punn=punn, user=request.user, vote='D')
-        vote.save()
-      elif v.count() == 1:
-        v[0].delete()
+      punnvote(request, 'D', punn)
       return HttpResponseRedirect( punn.get_absolute_url() )
 
 @login_required
-def voteup(request, id):
-      comment = get_object_or_404(Comment, id=id)
-      punn = comment.punn
-      v = CommentVote.objects.filter(comment=comment).filter(user=request.user).filter(vote='U')
-      if v.count() == 0:
-        vote = CommentVote(comment=comment, user=request.user, vote='U')
+def punnvote(request, side, punn):
+      if PunnVote.objects.filter(punn=punn).filter(user=request.user).exists():
+        v = PunnVote.objects.filter(punn=punn).filter(user=request.user)
+        if v[0].vote == side:
+          v[0].delete()
+        else:
+          v[0].delete()
+          vote = PunnVote(punn=punn, user=request.user, vote=side)
+          vote.save()
+      else: 
+        vote = PunnVote(punn=punn, user=request.user, vote=side)
         vote.save()
-      elif v.count() == 1:
-        v[0].delete() 
-      return HttpResponseRedirect( punn.get_absolute_url() )
 
 @login_required
-def votedown(request, id):
-      comment = get_object_or_404(Comment, id=id)
-      punn = comment.punn
-      v = CommentVote.objects.filter(comment=comment).filter(user=request.user).filter(vote='D')
-      if v.count() == 0:
-        vote = CommentVote(comment=comment, user=request.user, vote='D')
+def commentvote(request, side, comment):
+      if CommentVote.objects.filter(comment=comment).filter(user=request.user).exists():
+        v = CommentVote.objects.filter(comment=comment).filter(user=request.user)
+        if v[0].vote == side:
+          v[0].delete()
+        else:
+          v[0].delete()
+          vote = CommentVote(comment=comment, user=request.user, vote=side)
+          vote.save()
+      else: 
+        vote = CommentVote(comment=comment, user=request.user, vote=side)
         vote.save()
-      elif v.count() == 1:
-        v[0].delete() 
-      return HttpResponseRedirect( punn.get_absolute_url() )
+
+@login_required
+def commentup(request, id):
+      comment = get_object_or_404(Comment, id=id)
+      commentvote(request, 'U', comment)
+      return HttpResponseRedirect( comment.punn.get_absolute_url() )
+
+@login_required
+def commentdown(request, id):
+      comment = get_object_or_404(Comment, id=id)
+      commentvote(request, 'D', comment)
+      return HttpResponseRedirect( comment.punn.get_absolute_url() )
+
