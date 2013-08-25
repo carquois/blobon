def81f4b90195df1859347a07f6be009dc03e3a2
Gabriel Dancause <gabrieldancause@gmail.com>
2013-08-25 17:51:06 -0400

    #202 views

--- punn_it/earnings/views.py
+++ punn_it/earnings/views.py
@@ -3,7 +3,27 @@ from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 
 
+from django.conf import settings
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseNotFound
+from django.shortcuts import redirect, render_to_response, get_object_or_404
+from django.template import RequestContext
+from django.utils.translation import ugettext as _
+
 @login_required
+def index(request):
+      if request.META['HTTP_HOST'] == settings.MAIN_SITE_DOMAIN::
+          user = ""
+          punns = paginate(request,
+                           Punn.objects.filter(status='P').filter(is_top=True).order_by('-pub_date'),
+                           15)
+      else:
+        return redirect("http://knobshare.com")
+      return render_to_response('index.html',
+                               {'user': user,
+                                'punns': punns, },
+                                context_instance=RequestContext(request))
+
+
 def index(request):
       if request.META['HTTP_HOST'] == "knobshare.com":
           user = ""
