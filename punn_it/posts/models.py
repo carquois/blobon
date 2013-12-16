c3fec2575569de04f07162e74f65931fa954c422
Gabriel Dancause <gabrieldancause@gmail.com>
2013-12-15 21:33:07 -0500

    Added image to Post.Image

--- punn_it/posts/models.py
+++ punn_it/posts/models.py
@@ -16,13 +16,16 @@ STATUS = (
     ('D', 'Draft'),
 )
 
+def get_file_path(instance, filename):
+    ext = filename.split('.')[-1]
+    prefix = instance.base62id
+    filename = "%s.%s" % (prefix, ext)
+    return os.path.join('', filename)
 
 class Post(models.Model):
     author = models.ForeignKey(User, null=True)
     created = models.DateTimeField(auto_now_add = True)
     last_modified = models.DateTimeField(auto_now = True,  null=True, blank=True)
-    def __unicode__(self):
-        return self.title
     
 class BlogPost(Post):
     blog = models.ForeignKey(Blog, null=True)
@@ -30,3 +33,28 @@ class BlogPost(Post):
     content = models.TextField(verbose_name=_("Content"),max_length=10000, blank=True)
     pub_date = models.DateTimeField(auto_now_add = True, null=True, blank=True)
     status = models.CharField(max_length=2, choices=STATUS, default="P")
+    def __unicode__(self):
+        return self.title
+
+class Image(Post):
+    status = models.CharField(max_length=2, choices=STATUS, null=True, blank=True)
+    title = models.CharField(verbose_name=_("Titre"), max_length=140, blank=True)
+    translated_title = models.CharField(verbose_name=_("Titre traduit"), max_length=140, blank=True)
+    base62id = models.CharField(max_length=140, blank=True)
+    slug = models.SlugField(max_length=140, blank=True)
+    karma = models.IntegerField(default=0)
+    views = models.IntegerField(default=0, blank=True)
+    source = models.URLField(verbose_name=_("Source"), max_length=300, blank=True)
+    original_punn = models.ForeignKey('self',  null=True, blank=True)
+    content = models.TextField(verbose_name=_("Contenu"),max_length=10000, blank=True)
+    is_video = models.BooleanField(default=False)
+    is_top = models.BooleanField(default=False)
+    publish_on_facebook = models.BooleanField(default=False)
+    youtube_id = models.CharField(max_length=50, null=True, blank=True)
+    pic = ImageField(verbose_name=_("Image"), upload_to=get_file_path, null=True, blank=True)
+    def __unicode__(self):
+        return self.title
+
+
+
+
