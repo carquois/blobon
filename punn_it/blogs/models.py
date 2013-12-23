65c999e0bb5dcc695635be9b33553f3a5e4b47f8
Gabriel Dancause <gabrieldancause@gmail.com>
2013-12-23 15:36:49 -0500

    Added Category to blogs and remove categories app

--- punn_it/blogs/models.py
+++ punn_it/blogs/models.py
@@ -23,4 +23,14 @@ class Blog(models.Model):
     password = models.CharField(verbose_name=_("Password"), max_length=140, blank=True)
     def __unicode__(self):
         return self.title
-    
+
+class Category(models.Model):
+    author = models.ForeignKey(User)
+    blog = models.ForeignKey(Blog, null=True)
+    name = models.CharField(verbose_name=_("Name"), max_length=140)
+    description = models.CharField(verbose_name=_("Description"), max_length=1000)
+    slug = models.SlugField(max_length=140, unique=True)
+    created = models.DateTimeField(auto_now_add = True, null=True, blank=True)
+    top_level_cat = models.ForeignKey('self',  null=True, blank=True)
+    def __unicode__(self):
+        return self.slug    
