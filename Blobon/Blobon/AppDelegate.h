693f2bf55dc18afde6b03382128bf7c20e304692
Gabriel Dancause <gabrieldancause@gmail.com>
2014-02-02 21:18:58 +1300

    Initial commit for the iPhone app

---
+++ Blobon/Blobon/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Blobon
+//
+//  Created by Gabriel Dancause on 2014-02-02.
+//  Copyright (c) 2014 Gabriel Dancause. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
