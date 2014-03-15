e5e121492bd0ed59544cf738265d098d8fd22d36
Gabriel Dancause <gabrieldancause@gmail.com>
2014-03-15 10:06:45 -0400

    Created the books app and started the invoice system

---
+++ punn_it/books/migrations/0013_auto__chg_field_tax_rate.py
@@ -0,0 +1,131 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+
+        # Changing field 'Tax.rate'
+        db.alter_column('books_tax', 'rate', self.gf('django.db.models.fields.DecimalField')(max_digits=4, decimal_places=2))
+
+    def backwards(self, orm):
+
+        # Changing field 'Tax.rate'
+        db.alter_column('books_tax', 'rate', self.gf('django.db.models.fields.CommaSeparatedIntegerField')(max_length=4))
+
+    models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'books.client': {
+            'Meta': {'object_name': 'Client'},
+            'city': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
+            'country': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '100'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
+            'organization_name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'phone': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
+            'postal_code': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
+            'state': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
+            'street_adress': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'})
+        },
+        'books.expense': {
+            'Meta': {'object_name': 'Expense'},
+            'author': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        },
+        'books.invoice': {
+            'Meta': {'object_name': 'Invoice'},
+            'author': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"}),
+            'client': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['books.Client']"}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'date_of_issue': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'invoice_number': ('django.db.models.fields.PositiveIntegerField', [], {'blank': 'True'}),
+            'last_modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'null': 'True', 'blank': 'True'}),
+            'notes': ('django.db.models.fields.CharField', [], {'max_length': '1000'}),
+            'terms': ('django.db.models.fields.CharField', [], {'max_length': '1000'})
+        },
+        'books.item': {
+            'Meta': {'object_name': 'Item'},
+            'client': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['books.Client']", 'null': 'True'}),
+            'cost': ('django.db.models.fields.DecimalField', [], {'max_digits': '8', 'decimal_places': '2'}),
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '1000', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'quantity': ('django.db.models.fields.PositiveIntegerField', [], {'blank': 'True'})
+        },
+        'books.project': {
+            'Meta': {'object_name': 'Project'},
+            'client': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['books.Client']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'rate_per_hour': ('django.db.models.fields.DecimalField', [], {'max_digits': '5', 'decimal_places': '2'})
+        },
+        'books.task': {
+            'Meta': {'object_name': 'Task'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'project': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['books.Project']"}),
+            'rate_per_hour': ('django.db.models.fields.DecimalField', [], {'max_digits': '5', 'decimal_places': '2'})
+        },
+        'books.tax': {
+            'Meta': {'object_name': 'Tax'},
+            'compound_tax': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'gouv_number': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
+            'number': ('django.db.models.fields.PositiveIntegerField', [], {}),
+            'rate': ('django.db.models.fields.DecimalField', [], {'max_digits': '4', 'decimal_places': '2', 'blank': 'True'})
+        },
+        'books.time': {
+            'Meta': {'object_name': 'Time'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'invoice': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['books.Invoice']", 'null': 'True', 'blank': 'True'}),
+            'notes': ('django.db.models.fields.CharField', [], {'max_length': '1000'}),
+            'rate_per_hour': ('django.db.models.fields.DecimalField', [], {'max_digits': '5', 'decimal_places': '2'}),
+            'task': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['books.Task']", 'null': 'True', 'blank': 'True'}),
+            'time': ('django.db.models.fields.PositiveIntegerField', [], {'blank': 'True'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        }
+    }
+
+    complete_apps = ['books']
--- punn_it/books/views.py
+++ punn_it/books/views.py
@@ -1,1 +1,55 @@
-# Create your views here.
+# -*- coding: utf-8 -*-
+
+from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseNotFound
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import redirect, render_to_response, get_object_or_404
+from django.template import RequestContext
+from django.contrib import messages
+from django.utils.translation import ugettext as _
+from django.core.files.temp import NamedTemporaryFile
+import urllib2
+from urlparse import urlparse
+from django.core.files import File
+from cgi import parse_qs
+
+from books.models import Client, Project, Task, Time, Invoice, Tax, Expense, Item
+from django.contrib.auth.models import User
+
+@login_required
+def invoice(request, id):
+    invoice = get_object_or_404(Invoice, id=id)
+    client = invoice.client
+    times = Time.objects.filter(invoice=invoice)
+    taxs = Tax.objects.all()
+    items = Item.objects.filter(client=client)
+    subtotal_1 = 0
+    number_of_hours = 0
+    subtotal_2 = 0
+    sub_tax1 = 0
+    subtotal_3 = 0
+    sub_tax2 = 0
+    total = 0
+    for item in items:
+      subtotal_2 = subtotal_2 + item.cost
+    for time in times:
+      number_of_hours = number_of_hours + time.time
+      subtotal_1 = subtotal_1 + (time.time * time.rate_per_hour)
+    subtotal = subtotal_1 + subtotal_2
+    for tax in taxs:
+      if not tax.compound_tax:
+        tax1 = tax.rate
+        x = (tax1 * subtotal)/100
+        sub_tax1 = ("%.2f" % round(x,2))
+      else:
+        subtotal_3 = subtotal + x
+        tax2 = tax.rate
+        y = (tax2 * subtotal_3)/100
+        sub_tax2 = ("%.2f" % round(y,2))
+    z = subtotal_3 + y
+    total = ("%.2f" % round(z,2))
+    return render_to_response('invoice.html',
+                             {'invoice': invoice, 'client': client, 'times': times,'taxs': taxs, 
+                              'items': items,'total': total, 'sub_tax2':sub_tax2 ,'sub_tax1':sub_tax1 ,'subtotal': subtotal, 'subtotal_1': subtotal_1, 'subtotal_2': subtotal_2, 'number_of_hours': number_of_hours},
+                             context_instance=RequestContext(request))
