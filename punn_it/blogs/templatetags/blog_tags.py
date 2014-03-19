ac8e4d31ef0571667e7dc60c31d25ddf12bbb13d
Gabriel Dancause <gabrieldancause@gmail.com>
2014-03-19 10:10:06 -0400

    Started #404

--- punn_it/blogs/templatetags/blog_tags.py
+++ punn_it/blogs/templatetags/blog_tags.py
@@ -1,19 +1,35 @@
 from django import template
-register = template.Library()
+import re
 
-class UltraRender(template.Node):
+#re_nobr = re.compile("<br />")
+#register = template.Library()
 
-    def __init__(self, obj_and_field):
-        self.obj = template.Variable(obj_and_field.split('.')[0])
-        self.t = obj_and_field.split('.')[1]
+#def do_nobr(parser, token):
+#	nodelist = parser.parse(('endnobr',))
+#	parser.delete_first_token()
+#	return NobrNode(nodelist)
 
-    def render(self, context):
-        obj = self.obj.resolve(context)
-        t = template.Template(getattr(obj, self.t))
-        rendered_field = t.render(context)
-        return template.defaultfilters.linebreaks(rendered_field)
+#class NobrNode(template.Node):
+#    def __init__(self, nodelist):
+#        self.nodelist = nodelist
+#    def render(self, context):
+#        rendered = self.nodelist.render(context).strip()
+#        return re_nobr.sub("", rendered)
 
-@register.tag
-def ultrarender(parser, token):
-    bits = token.contents.split()
-    return UltraRender(bits[1])
+
+#class UltraRender(template.Node):
+
+#    def __init__(self, obj_and_field):
+#        self.obj = template.Variable(obj_and_field.split('.')[0])
+#        self.t = obj_and_field.split('.')[1]
+
+ #   def render(self, context):
+ #       obj = self.obj.resolve(context)
+  #      t = template.Template(getattr(obj, self.t))
+  #      rendered_field = t.render(context)
+  #      return template.defaultfilters(rendered_field)
+
+#@register.tag
+#def ultrarender(parser, token):
+#    bits = token.contents.split()
+#    return UltraRender(bits[1])
