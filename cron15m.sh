df29720d39c7058475b7db65134840f20a309572
Gabriel Dancause <gabrieldancause@gmail.com>
2014-03-21 17:22:31 -0400

    Test cron

--- cron15m.sh
+++ cron15m.sh
@@ -2,4 +2,4 @@
 /home/web/punn.it/bin/python /home/web/punn.it/punn_it/manage.py publish 15m
 #cd /home/web/punn.it/punn_it/punns/management/commands
 #/home/web/punn.it/bin/python /home/web/punn.it/punn_it/manage.py countviews 100
-
+#À revoir
